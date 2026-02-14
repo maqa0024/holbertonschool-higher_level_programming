3504666693a636e22a0ead0dc154cf1c8fdddcaf
Maqa0024 <Mahammadlimammadli@gmail.com>
2026-02-14 14:05:20 +0000

    asd

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    print("{:s}".format(chr(i)) if chr(i) not in "eq" else "", end="")
+    if chr(i) != 'q' and chr(i) != 'e':
+        print("{}".format(chr(i)), end="")
