c6243ab0482ca86034564191cae731bf27f647ce
Maqa0024 <Mahammadlimammadli@gmail.com>
2026-03-15 18:20:28 +0000

    sql-more

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,11 +1,4 @@
--- List all cities of California using a subquery (no JOIN)
-USE hbtn_0d_usa;
-
-SELECT id, name
-FROM cities
-WHERE state_id = (
-    SELECT id
-    FROM states
-    WHERE name = 'California'
-)
+-- 8-cities_of_california_subquery.sql
+SELECT id, name FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;
--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
--- Lists all cities with their state names from hbtn_0d_usa
-SELECT cities.id, cities.name AS city, states.name AS state
+-- 9-cities_by_state_join.sql
+SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states ON cities.state_id = states.id
-ORDER BY cities.id;
+ORDER BY cities.id ASC;
