427d25d8e951768ab7873b8d981cf3bb3c18dd44
Maqa0024 <Mahammadlimammadli@gmail.com>
2026-03-15 18:35:28 +0000

    python -sql

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Lists all states with a name starting with N from hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cursor.fetchall():
+        if row[1][0] == 'N':
+            print(row)
+    cursor.close()
+    db.close()
---
+++ python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+salam sag ol 
