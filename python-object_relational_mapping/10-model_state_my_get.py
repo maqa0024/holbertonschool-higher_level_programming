cd513a2c76f08ca38c24e9cc78169178ebf33751
Maqa0024 <Mahammadlimammadli@gmail.com>
2026-03-15 19:40:19 +0000

    python -obj son49

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,37 +1,24 @@
 #!/usr/bin/python3
 """
-Prints the State object with the name passed as argument 
-from the database hbtn_0e_6_usa.
+prints the State object with the name passed as argument from a database
 """
-import sys
-from model_state import Base, State
+
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from sys import argv
+from model_state import Base, State
 
 if __name__ == "__main__":
-    # 1. Connect to the database using arguments provided
-    # Syntax: mysql+mysqldb://user:password@host/db
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True
-    )
-
-    # 2. Create a configured "Session" class and instance
-    Session = sessionmaker(bind=engine)
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
     session = Session()
-
-    # 3. Query the State object
-    # We use .filter() with the fourth command line argument
-    # SQLAlchemy handles the parameterization to prevent SQL injection
-    state = session.query(State).filter(State.name == sys.argv[4]).first()
-
-    # 4. Display results
-    if state:
-        print("{}".format(state.id))
+    state = session.query(State).filter_by(name=argv[4]).first()
+    if state is not None:
+        print(str(state.id))
     else:
         print("Not found")
-
-    # 5. Close the session
     session.close()
